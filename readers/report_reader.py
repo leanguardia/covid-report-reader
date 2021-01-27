1e85b1f77145a6c5d265a5accb7900dd742db05d
Leandro Guardia <lean.guardia93@gmail.com>
2021-01-27 01:12:53 +0000

    Extract New Cases Today

--- readers/report_reader.py
+++ readers/report_reader.py
@@ -1,7 +1,24 @@
 import cv2
-import pandas as pd
 import pytesseract as ocr
 
-class ReportReader():
-    def read(self, report_img):
-        return pd.DataFrame()
+def grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+def threshold(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+class ReportReader:
+    def __init__(self, report_img):
+        self.img = threshold(grayscale(report_img))
+
+    def read(self):
+        # custom_config = r'--oem 3 --psm 6'
+        string = ocr.image_to_data(self.departments_img())#, config=custom_config)
+        rows = string.split('\n')
+        elements = [row.split('\t') for row in rows]
+        return {
+            "new_cases": elements[5][-1]
+        }
+    
+    def departments_img(self):
+        return self.img[250:, :1518]
--- test/test_read_report.py
+++ test/test_read_report.py
@@ -1,14 +1,17 @@
 from readers.report_reader import ReportReader
 
 import cv2
-import pandas as pd
 from unittest import TestCase
 
 class TestReportReader(TestCase):
     def setUp(self):
-        self.report_img = cv2.imread('fixtures/210125-Rep-COVID-316-2030-01-scaled.jpg')
-        self.reader = ReportReader()
+        self.report_img = cv2.imread("test/fixtures/210125-Rep-COVID-316-2030-01-scaled.jpg")
+        self.reader = ReportReader(self.report_img)
+
+    def test_returns_a_dictionary(self):
+        report = self.reader.read()
+        self.assertIsInstance(report, dict)
 
     def test_returns_a_dataframe(self):
-        report = self.reader.read(self.report_img)
-        self.assertIsInstance(report, pd.DataFrame) 
+        report = self.reader.read()
+        self.assertEqual(report["new_cases"], "1.781")
