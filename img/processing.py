5ed7c4c1c9472367c07dd7c309f49d8c7bcde0a3
Leandro Guardia <lean.guardia93@gmail.com>
2021-02-11 11:43:31 +0000

    Cover Report Corners

--- img/processing.py
+++ img/processing.py
@@ -1,6 +1,12 @@
 import cv2
 
 def process(image):
+    height, width, _ = image.shape
+    
+    white_square(image, (0, height - 150), (490, height)) # Vamos a salir adelante
+    white_square(image, (width - 350, 0),  (width, 350)) # Logo Min. de Salud
+    # white_square(image, (378, 500), (1010, 1200)) # Map Lines
+    # white_square(image, (1705, 700), (1835, 780)) # Alert Sign (suspicious)
     return threshold(grayscale(image))
 
 def grayscale(image):
@@ -8,3 +14,6 @@ def grayscale(image):
 
 def threshold(image):
     return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+def white_square(image, top_left, bottom_right):
+    return cv2.rectangle(image, top_left, bottom_right, (255, 255, 255), -1)
--- sandbox.py
+++ sandbox.py
@@ -10,8 +10,8 @@ def show_img(img):
     cv2.destroyAllWindows()
 
 img = cv2.imread('test/fixtures/210125-Rep-COVID-316-2030-01-scaled.jpg')
+print("Shape:", img.shape)
 processed = process(img)
-# show_img(img)
 
 # departments = img[250:, :1518]
 # cases_today = img[250:420, :1518]
@@ -23,9 +23,9 @@ processed = process(img)
 # img = thresholding(img)
 # show_img(img)
 
+string = ocr.image_to_data(processed)
 # conf = r'--oem 3 --psm 6'
-# string = ocr.image_to_data(img, config=conf)
-string = ocr.image_to_data(processed)#, config=conf)
+# string = ocr.image_to_data(processed, config=conf)
 print(string)
 
 rows = OcrDataParser().parse(string)
@@ -38,11 +38,10 @@ print("ROWS: {}".format(len(rows)))
 
 for row in rows[1:-1]:
     # print(row.text())
-
     top_left = (row.left(), row.top())
-    print(top_left)
+    # print(top_left)
     bottom_right  = (row.left() + row.width(), row.top() + row.height())
-    print(bottom_right)
-    img = cv2.rectangle(img, top_left, bottom_right, (0, 255, 0), 2)
-show_img(img)
+    # print(bottom_right)
+    processed = cv2.rectangle(processed, top_left, bottom_right, (0, 0, 0), 2)
+show_img(processed)
 
