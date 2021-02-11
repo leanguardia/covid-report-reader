e27aac1addeed4125ef5f28e639196b92e6deadf
Leandro Guardia <lean.guardia93@gmail.com>
2021-02-11 11:03:16 +0000

    Sandbox draw rectangles

--- readers/parsers.py
+++ readers/parsers.py
@@ -3,11 +3,23 @@ class OcrDataParser:
         rows = ocr_data.split('\n')
         # print(ocr_data)
         # print(type(ocr_data))
-        return [OcrDataRow(row) for row in rows] 
+        return [OcrDataItem(row) for row in rows] 
 
-class OcrDataRow:
+class OcrDataItem:
     def __init__(self, raw):
         self.elements = raw.split('\t')
 
     def text(self):
         return self.elements[-1]
+
+    def left(self):
+        return int(self.elements[-6])
+
+    def top(self):
+        return int(self.elements[-5])
+
+    def width(self):
+        return int(self.elements[-4])
+
+    def height(self):
+        return int(self.elements[-3])
