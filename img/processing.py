b4c83c7b4816f73254e59deba15f0e9f08f4cdd2
Leandro Guardia <lean.guardia93@gmail.com>
2021-02-14 03:20:43 +0000

    Move Show Image

--- img/processing.py
+++ img/processing.py
@@ -1,5 +1,10 @@
 import cv2
 
+def show_img(img):
+    cv2.imshow('image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 def process(image):
     height, width, _ = image.shape
     
--- sandbox.py
+++ sandbox.py
@@ -8,26 +8,21 @@ from readers.parsers import OcrDataParser
 from readers.report_reader import ReportReader
 from img.processing import process#, grayscale, threshold 
 
-def show_img(img):
-    cv2.imshow('image', img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
 
 reports_path = 'img/reportes_salud/'
+
 _, _, filenames = next(walk(reports_path))
 # filenames = ['1353866415820955648_0.png']
 
 failed_files = ['.DS_Store',
+          '1339026608439627778_0.png',
+          '1338657548040286210_0.png',
+          '1340836592127074304_0.png',
+          '1355681007500976133_0.png',
+          '1347715466790363136_0.png',
           '1343723603758604292_0.png',
-          '1350611856755470338_0.png',
-          '1348071689251393540_0.png',
-          '1337599696790020096_0.png',
-          '1343007009491329029_0.png',
-          '1345896948281257984_0.png',
-          '1354235109247954945_0.png',
-          '1339744745611022341_0.png',
-          '1346620363992662020_0.png',
-          '1344487124720996352_0.png',
+          '1356409086234009602_0.png',
+          '1341198161566720001_0.png',
         ]
 
 df_data = {
@@ -57,27 +52,15 @@ for filename in filenames:
     img = cv2.imread(reports_path + filename)
     # show_img(img)
     print("Shape:", img.shape)
-    reader = ReportReader(img)
-    report = reader.read()
-    print(report)
-    df_data['number'].append(report['number'])
-    df_data['date'].append(report['date'])
-    df_data['time'].append(report['time'])
-    df_data['new_cases'].append(report['new_cases'])
-    df_data['new_deaths'].append(report['new_deaths'])
-    df_data['recovered'].append(report['recovered'])
-    df_data['active'].append(report['active'])
-    df_data['deaths'].append(report['deaths'])
-    df_data['suspicious'].append(report['suspicious'])
-    df_data['accumulated'].append(report['accumulated'])
-    df_data['discarted'].append(report['discarted'])
-    df_data['beni'].append(report['beni'])
-    df_data['chuquisaca'].append(report['chuquisaca'])
-    df_data['cochabamba'].append(report['cochabamba'])
-    df_data['la_paz'].append(report['la_paz'])
-    df_data['pando'].append(report['pando'])
-    df_data['santa_cruz'].append(report['santa_cruz'])
-    df_data['tarija'].append(report['tarija'])
+    processed = process(img)
+    reader = ReportReader(processed)
+    try:
+      report = reader.read()
+      print(report)
+      accumulate_report(report)
+    except IndexError as e:
+      print("Failed " + filename)
+
 
 print(df_data)
 df = pd.DataFrame(df_data)
