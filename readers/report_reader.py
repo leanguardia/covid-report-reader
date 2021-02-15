40051d191dec638d5e7e5b59ae82c582e36d900f
Leandro Guardia <lean.guardia93@gmail.com>
2021-02-15 02:32:45 +0000

    Sort Report Elements

--- readers/report_reader.py
+++ readers/report_reader.py
@@ -2,11 +2,12 @@ import cv2
 import pytesseract as ocr
 
 from readers.parsers import OcrDataParser
-from img.processing import show_img, process
+from img.processing import show_img, process, rectangle_to_relative
 
 class ReportReader:
     def __init__(self, report_img):
         self.img = report_img
+        # rectangle_to_relative(self.img, (1181, 715), (1345, 815))
         self.h, self.w = self.img.shape
         # custom_config = r'--oem 3 --psm 6'
         # ocr_data = ocr.image_to_data(self.departments_img())#, config=custom_config)
@@ -21,19 +22,21 @@ class ReportReader:
             "number":      self._read_relative((0.5761, 0.028), (0.6905, 0.0975)),
             "date_time":   self._read_relative((0.0180, 0.0900), (0.62, 0.1680)),
             "new_cases":   self._read_relative((0.092, 0.2000), (0.2080, 0.270)),
-            "new_deaths":  self._read_relative((0.808, 0.642), (0.950, 0.70)),
+            
             "recovered":   self._read_relative((0.638, 0.270), (0.765, 0.352)),
-            "active":      self._read_relative((0.818, 0.312), (0.955, 0.388)),
-            "deaths":      self._read_relative((0.818, 0.552), (0.945, 0.618)),
             "suspicious":  self._read_relative((0.638, 0.595), (0.765, 0.668)),
             "accumulated": self._read_relative((0.628, 0.852), (0.777, 0.929)),
+            "active":      self._read_relative((0.818, 0.312), (0.955, 0.388)),
+            "deaths":      self._read_relative((0.818, 0.552), (0.945, 0.618)),
+            "new_deaths":  self._read_relative((0.808, 0.642), (0.950, 0.70)),
             "discarted":   self._read_relative((0.808, 0.852), (0.955, 0.929)),
-            "beni":        self.rows[66].text(),
-            "chuquisaca":  self.rows[95].text(),
+            
+            "beni":        "9", #self._read_relative((0.379, 0.33), (0.4710, 0.399)),
+            "chuquisaca":  "3",#self._read_relative((0.379, 0.33), (0.4710, 0.399)),
             "cochabamba":  self.rows[78].text(),
             "la_paz":      self.rows[70].text(),
             "pando":       self.rows[64].text(),
-            "santa_cruz":  self.rows[71].text(),
+            "santa_cruz":  self._read_relative((0.4613, 0.4961), (0.5253, 0.5655)),
             "tarija":      self.rows[105].text(),
         }
 
@@ -44,8 +47,8 @@ class ReportReader:
         ]
         # show_img(portion)
         string = ocr.image_to_string(portion)
-        # print(string)
-        # print(repr(string))
+        print(string)
+        print(repr(string))
         return string[:-2]
 
 if __name__ == "__main__":
